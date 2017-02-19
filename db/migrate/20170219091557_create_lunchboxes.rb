f8de407bb5647b9eba03736e9de24cf3dc59281f
color_box <colorbox222@gmail.com>
2017-02-19 19:07:44 +0900

    Add main migrate files

--- db/migrate/20170219091557_create_lunchboxes.rb
+++ db/migrate/20170219091557_create_lunchboxes.rb
@@ -2,6 +2,9 @@ class CreateLunchboxes < ActiveRecord::Migration[5.0]
   def change
     create_table :lunchboxes do |t|
 
+      t.integer :type
+      t.integer :price
+
       t.timestamps
     end
   end
--- db/migrate/20170219091612_create_orders.rb
+++ db/migrate/20170219091612_create_orders.rb
@@ -2,6 +2,10 @@ class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
 
+      t.integer :lunckbox_id
+      t.string :customer_name
+      t.date :orderd_on
+
       t.timestamps
     end
   end
