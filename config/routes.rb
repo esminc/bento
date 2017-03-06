291306f361452ea42c78d281c5843f478cc5a1cd
Takumi Kaji <ivezuki3@gmail.com>
2017-03-06 23:56:11 +0900

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :orders, only: %i(index) do
-    resources :order_items, only: %i(new create show)
+  namespace :admin do
+    resources :orders, only: %i(show) do
+      patch :submit
+    end
+  end
+
+  resources :orders, only: %i(index show) do
+    get :confirm
+
+    resources :order_items, only: %i(new create edit update destroy) do
+      patch :receive
+    end
   end
 end
