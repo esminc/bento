e9718ef5f656c35f4ae3fa86266b45adbca00eef
Takumi Kaji <ivezuki3@gmail.com>
2017-03-11 20:34:16 +0900

    Use I18n

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,5 +1,5 @@
 module OrdersHelper
   def closed_info(order)
-    "注文締め切り時間: #{order.closed_at}"
+    "注文締め切り時間: #{I18n.l(order.closed_at)}"
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Bento
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
