4a97f955dc5ea5e89e57ae2ab57720ebec0e7813
colorbox <colorbox222@gmail.com>
2017-03-12 17:40:19 +0900

    Add test for edit order

--- spec/features/order/order_order_items_spec.rb
+++ spec/features/order/order_order_items_spec.rb
@@ -30,6 +30,28 @@ RSpec.feature "Order::OrderItems", type: :feature do
   end
 
 
+  feature '注文の修正' do
+    scenario '注文者は自分の注文を修正できる' do
+      create(:lunchbox)
+      order = create(:order)
+      order_item = create(:order_item)
+
+      visit order_order_items_path(order)
+      expect(page).to have_text(order_item.customer_name)
+
+      # edit name
+      click_link(order_item.customer_name)
+      expect(page).not_to have_text(order_item.customer_name)
+      fill_in "Customer name", with:"another_name"
+
+      # update confirm
+      click_button "Update Order item"
+      expect(page).not_to have_text(order_item.customer_name)
+      expect(page).to have_text("another_name")
+
+    end
+  end
+
 end
 
 
