0e3ea94cee69832747ab7d3df25f5287b1ed2985
colorbox <colorbox222@gmail.com>
2017-03-15 21:15:05 +0900

    Add test for new action restriction

--- spec/features/order/order_order_items_spec.rb
+++ spec/features/order/order_order_items_spec.rb
@@ -13,6 +13,20 @@ RSpec.feature "Order::OrderItems", type: :feature do
     end
   end
 
+  feature '注文の新規作成' do
+    scenario 'Order が締め切られている場合、注文者は新しく注文できない' do
+      lunchbox = create(:lunchbox)
+      order = create(:order, closed_at: Time.zone.local(2017, 2, 1))
+
+      visit order_order_items_path(order)
+
+      click_link('予約する')
+      # インデックスに戻される
+      expect(page).to have_text('予約する')
+
+    end
+  end
+
   feature '注文のキャンセル' do
     scenario '注文者は自分の注文をキャンセルできる' do
       lunchbox = create(:lunchbox)
