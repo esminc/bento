d6cb01490721c11c9b6549a7571663eeae373e17
Yuka Kato <satoimo.to.imo@gmail.com>
2017-03-18 14:21:56 +0900

    Sunday and Saturday are more than holidays in a year.

--- lib/tasks/rake_helpers.rb
+++ lib/tasks/rake_helpers.rb
@@ -1,8 +1,8 @@
 def next_weekday_of(date)
   next_date = date.tomorrow
   # NOTE: 平日とは、土・日・祝日いずれでもないものとする
-  return next_date unless HolidayJp.holiday?(next_date) \
-                            || next_date.sunday? \
-                            || next_date.saturday?
+  return next_date unless next_date.sunday? \
+                          || next_date.saturday? \
+                          || HolidayJp.holiday?(next_date)
   next_weekday_of(next_date)
 end
