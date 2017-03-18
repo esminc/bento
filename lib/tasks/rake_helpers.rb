2b89a27567620811e31da890b162822d36c9daa2
Yuka Kato <satoimo.to.imo@gmail.com>
2017-03-18 15:32:03 +0900

    I hate unless (with multiple conditions), which makes us confused

--- lib/tasks/rake_helpers.rb
+++ lib/tasks/rake_helpers.rb
@@ -1,8 +1,8 @@
 def next_weekday_of(date)
   next_date = date.tomorrow
   # NOTE: 平日とは、土・日・祝日いずれでもないものとする
-  return next_date unless next_date.sunday? \
-                          || next_date.saturday? \
-                          || HolidayJp.holiday?(next_date.to_date)
+  return next_date if !next_date.sunday? \
+                      && !next_date.saturday? \
+                      && !HolidayJp.holiday?(next_date.to_date)
   next_weekday_of(next_date)
 end
