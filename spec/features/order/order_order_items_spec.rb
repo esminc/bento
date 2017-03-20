ff7ea4de6c429c9f7e39126a87229e122dd46825
colorbox <colorbox222@gmail.com>
2017-03-21 02:07:37 +0900

    Add restriction for create order item when order closed

--- spec/features/order/order_order_items_spec.rb
+++ spec/features/order/order_order_items_spec.rb
@@ -25,6 +25,35 @@ RSpec.feature "Order::OrderItems", type: :feature do
       expect(page).to have_text('予約する')
 
     end
+
+    scenario 'Order が締め切られている場合、注文者は新しい注文を確定できない' do
+      lunchbox = create(:lunchbox)
+      order = create(:order)
+
+      visit order_order_items_path(order)
+      user_name = 'sample-user'
+
+      # 未予約なことを確認
+      expect(page).not_to have_text(user_name)
+
+      # 予約ページで情報を入力
+      click_link('予約する')
+      fill_in 'Customer name', with: 'customer'
+      select 'sample弁当', from: 'lunchbox[id]'
+
+      # 弁当発注の確定
+      order.closed_at = Time.zone.local(2017, 2, 1)
+      order.save
+
+      # ユーザーが自分の注文を確定しようとする
+      click_button 'Create Order item'
+
+      # インデックスに戻され、予約は確定できていない
+      expect(page).not_to have_text(user_name)
+      expect(page).to have_text('予約する')
+
+    end
+
   end
 
   feature '注文のキャンセル' do
