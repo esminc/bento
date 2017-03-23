c92b3fd12d87fc7fe7a493eaac128ce429dead82
colorbox <colorbox222@gmail.com>
2017-03-24 01:04:15 +0900

    Add basic authorization actionon controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :basic_authorization
+
+  private
+
+  def basic_authorization
+    authenticate_or_request_with_http_basic do |user, pass|
+      user == ENV['USER_ID'] && pass == ENV['USER_PASS']
+    end
+  end
+
 end
