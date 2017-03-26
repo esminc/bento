7b7c3faba38450d32f8b1ad6d5713f198b684769
Yuka Kato <satoimo.to.imo@gmail.com>
2017-03-26 20:30:38 +0900

    Fix type

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    @today_order = Order.find_by(date: Time.current)
+    @today_order = Order.find_by(date: Time.current.to_date)
     @orders = Order.available
   end
 end
