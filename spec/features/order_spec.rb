2eecaed8f90ad354da6595c9630506673d713695
Takumi Kaji <ivezuki3@gmail.com>
2017-03-27 02:10:37 +0900

    Fix broken spec

--- spec/features/order_spec.rb
+++ spec/features/order_spec.rb
@@ -99,10 +99,10 @@ RSpec.feature 'Order', type: :feature do
 
   feature '注文の修正' do
     scenario '注文者は自分の注文を修正できる' do
-      create(:lunchbox, name: 'sample弁当-上')
+      other_lunchbox = create(:lunchbox, name: 'sample弁当-上')
       order_item = create(:order_item, order: order, lunchbox: lunchbox)
       new_name = 'another_name'
-      new_lunchbox_name = 'sample弁当-上'
+      new_lunchbox_tag = "#{other_lunchbox.name} (#{other_lunchbox.price}円)"
 
       visit order_order_items_path(order)
       expect(page).to have_text(order_item.customer_name)
@@ -110,8 +110,8 @@ RSpec.feature 'Order', type: :feature do
       click_link(order_item.customer_name)
       fill_in '名前', with: new_name
 
-      expect(page).to have_select('order_item[lunchbox_id]',selected: lunchbox.name)
-      select new_lunchbox_name, from: 'order_item[lunchbox_id]'
+      expect(page).to have_select('order_item[lunchbox_id]', selected: lunchbox.decorate.price_tag)
+      select new_lunchbox_tag, from: 'order_item[lunchbox_id]'
 
       click_button '注文を確定する'
 
@@ -119,7 +119,7 @@ RSpec.feature 'Order', type: :feature do
       expect(page).to have_text(new_name)
 
       click_link(new_name)
-      expect(page).to have_select('order_item[lunchbox_id]',selected: new_lunchbox_name)
+      expect(page).to have_select('order_item[lunchbox_id]', selected: new_lunchbox_tag)
     end
 
     scenario 'Order が締め切られている場合、注文者は自分の注文を編集できない' do
