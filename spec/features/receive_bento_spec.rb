30f948fffa157c870079c6b602ba242569b01443
Yuka Kato <satoimo.to.imo@gmail.com>
2017-03-31 16:12:54 +0900

    Convert old order_spec.rb into receive_bento_spec as I have extracted all other test cases to new files

--- spec/features/receive_bento_spec.rb
+++ spec/features/receive_bento_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.feature 'Order', type: :feature do
+RSpec.feature '予約した弁当の受け取り', type: :feature do
   given!(:lunchbox) { create(:lunchbox) }
   given(:order) { create(:order) }
 
   feature '予約の受取' do
     given(:order) { create(:order, :closed) }
 
-    scenario '予約者は自分の予約した弁当を受け取ったことをシステムに知らせることが出来る' do
+    scenario '予約した弁当を受け取ったことをシステムに登録する' do
       create(:order_item, order: order, lunchbox: lunchbox)
 
       visit order_order_items_path(order)
