1981c766cf876c2483c4e1508ae2fedaa25d85ed
colorbox <colorbox222@gmail.com>
2017-04-03 22:20:04 +0900

    Refactor test for readability

--- spec/features/order_item_shorage_spec.rb
+++ spec/features/order_item_shorage_spec.rb
@@ -5,11 +5,16 @@ RSpec.feature '予約数が規定の数に足りない時', type: :feature do
   given(:order) { create(:order) }
 
   feature '確定された場合' do
-    scenario 'トップページにメッセージが表示される' do
+
+    before(:each) do
       Timecop.freeze(order.date) do
         create_list(:order_item, 2, order: order, lunchbox: lunchbox)
-
         order.close(Time.zone.local(2017, 2, 1))
+      end
+    end
+
+    scenario 'トップページにメッセージが表示される' do
+      Timecop.freeze(order.date) do
         visit orders_path
         expect(page).to have_text('注文数不足のため注文されませんでした')
       end
@@ -17,9 +22,6 @@ RSpec.feature '予約数が規定の数に足りない時', type: :feature do
 
     scenario '注文の確認画面へ遷移しようとするとトップページに戻される' do
       Timecop.freeze(order.date) do
-        create_list(:order_item, 2, order: order, lunchbox: lunchbox)
-
-        order.close(Time.zone.local(2017, 2, 1))
         visit order_order_items_path(order)
         expect(page).to have_text('注文数不足のため注文されませんでした')
       end
