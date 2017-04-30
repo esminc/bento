4bd552c0a67f98bb0f61e2033d8e293862a0f17f
Yuka Kato <satoimo.to.imo@gmail.com>
2017-05-01 01:27:36 +0900

    Idobata#post の汎用性を保持したいのでエンドポイントは外から渡すようにした

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,9 +6,9 @@ class Admin::OrdersController < Admin::ApplicationController
     order.close!
 
     if order.item_count_satisfied?
-      Idobata.post_for_user "本日(#{I18n.l(order.date)})分の注文が成立しました:smiley:今日はお弁当があります:bento:"
+      Idobata.post("本日(#{I18n.l(order.date)})分の注文が成立しました:smiley:今日はお弁当があります:bento:", ENV['IDOBATA_DEVELOPER_HOOK_URL'])
     else
-      Idobata.post_for_user "本日(#{I18n.l(order.date)})分の注文は成立しませんでした:cry:今日のランチは外に食べに行きましょう:fork_and_knife:"
+      Idobata.post("本日(#{I18n.l(order.date)})分の注文は成立しませんでした:cry:今日のランチは外に食べに行きましょう:fork_and_knife:", ENV['IDOBATA_USER_HOOK_URL'])
     end
 
     redirect_to todays_order_admin_orders_path, notice: '予約を締め切りました'
--- lib/idobata.rb
+++ lib/idobata.rb
@@ -1,13 +1,7 @@
 module Idobata
-  def post_for_developer(message)
-    url = ENV['IDOBATA_DEVELOPER_HOOK_URL']
+  def post(message, url)
     Faraday.post(url, source: message, format: 'html') if url.present?
   end
 
-  def post_for_user(message)
-    url = ENV['IDOBATA_USER_HOOK_URL']
-    Faraday.post(url, source: message, format: 'html') if url.present?
-  end
-
-  module_function :post_for_developer, :post_for_user
+  module_function :post
 end
