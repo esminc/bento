4ce20218c6ee9a50185339de33f0d568dc985129
Yuka Kato <satoimo.to.imo@gmail.com>
2017-05-01 01:32:47 +0900

    重複する部分をまとめた

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,11 +5,12 @@ class Admin::OrdersController < Admin::ApplicationController
     order = Order.find(params[:id])
     order.close!
 
-    if order.item_count_satisfied?
-      Idobata.post("本日(#{I18n.l(order.date)})分の注文が成立しました:smiley:今日はお弁当があります:bento:", ENV['IDOBATA_DEVELOPER_HOOK_URL'])
-    else
-      Idobata.post("本日(#{I18n.l(order.date)})分の注文は成立しませんでした:cry:今日のランチは外に食べに行きましょう:fork_and_knife:", ENV['IDOBATA_USER_HOOK_URL'])
-    end
+    message, url = if order.item_count_satisfied?
+                     ["本日(#{I18n.l(order.date)})分の注文が成立しました:smiley:今日はお弁当があります:bento:", ENV['IDOBATA_DEVELOPER_HOOK_URL']]
+                   else
+                     ["本日(#{I18n.l(order.date)})分の注文は成立しませんでした:cry:今日のランチは外に食べに行きましょう:fork_and_knife:", ENV['IDOBATA_USER_HOOK_URL']]
+                   end
+    Idobata.post(message, url)
 
     redirect_to todays_order_admin_orders_path, notice: '予約を締め切りました'
   end
