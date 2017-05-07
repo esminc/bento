ded47e064009f0a1a183c8a3ad7a9189f84f97f4
Yuka Kato <satoimo.to.imo@gmail.com>
2017-05-08 02:07:02 +0900

    通知と保存は一連の操作であって、片方だけが成功したという状態ではアプリケーションを使う人が誤解するきっかけになりかねないため、トランザクションで囲った。

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,17 +3,18 @@ require 'idobata'
 class Admin::OrdersController < Admin::ApplicationController
   def close
     order = Order.find(params[:id])
-    order.close!
 
-    message, url = if order.item_count_satisfied?
-                     ["本日(#{I18n.l(order.date)})分の注文が成立しました:smiley:今日はお弁当があります:bento:",
-                      ENV['IDOBATA_USER_HOOK_URL']]
-                   else
-                     ["本日(#{I18n.l(order.date)})分の注文は成立しませんでした:cry:今日のランチは外に食べに行きましょう:fork_and_knife:",
-                      ENV['IDOBATA_USER_HOOK_URL']]
-                   end
-
-    Idobata.post(message, url) if Rails.application.config.x.enable_idobata_notification
+    Order.transaction do
+      order.close!
+      message, url = if order.item_count_satisfied?
+                       ["本日(#{I18n.l(order.date)})分の注文が成立しました:smiley:今日はお弁当があります:bento:",
+                        ENV['IDOBATA_USER_HOOK_URL']]
+                     else
+                       ["本日(#{I18n.l(order.date)})分の注文は成立しませんでした:cry:今日のランチは外に食べに行きましょう:fork_and_knife:",
+                        ENV['IDOBATA_USER_HOOK_URL']]
+                     end
+      Idobata.post(message, url) if Rails.application.config.x.enable_idobata_notification
+    end
 
     redirect_to todays_order_admin_orders_path, notice: '予約を締め切りました'
   end
