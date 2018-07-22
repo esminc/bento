61b7967937cd920529ffaea5386b91130f9a0c2c
colorbox <colorbox222@gmail.com>
2018-07-22 17:56:28 +0900

    :policeman:

--- spec/features/admin/list_orders_spec.rb
+++ spec/features/admin/list_orders_spec.rb
@@ -5,7 +5,6 @@ RSpec.feature '注文可能日の一覧', type: :feature do
   given!(:order2) { create(:order, date: 'Fri, 2 Jun 2017', closed_at: nil) }
   given!(:order3) { create(:order, date: 'Mon, 5 Jun 2017', closed_at: nil) }
 
-
   scenario '日付を一覧表示できる' do
     Timecop.freeze(order1.date) do
       visit admin_orders_path
