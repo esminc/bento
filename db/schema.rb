d85fc9379e61885cd82f2ba4139327da3d0c3e66
Takumi Kaji <ivezuki3@gmail.com>
2019-08-12 20:15:20 +0900

    コンテナ上で rake db:migrate した際のdiffを加えた
    id がserial型であることが明示されたことについて特に問題ないと判断したので入れた。

--- db/schema.rb
+++ db/schema.rb
@@ -1,26 +1,39 @@
-ActiveRecord::Schema.define(version: 20170223160854) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2017_02_23_160854) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "lunchboxes", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.integer  "price",      null: false
+  create_table "lunchboxes", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "order_items", force: :cascade do |t|
-    t.integer  "order_id",      null: false
-    t.integer  "lunchbox_id",   null: false
-    t.string   "customer_name", null: false
+  create_table "order_items", id: :serial, force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "lunchbox_id", null: false
+    t.string "customer_name", null: false
     t.datetime "received_at"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["order_id"], name: "index_order_items_on_order_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.date     "date",       null: false
+  create_table "orders", id: :serial, force: :cascade do |t|
+    t.date "date", null: false
     t.datetime "closed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
